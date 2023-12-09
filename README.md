1eed17090ce00bd145f425ac791f1dc76bd318d8
Nicholas Hébert <68243838+n-hebert@users.noreply.github.com>
2023-12-09 15:23:22 -0400

    Revise to use sed and solve problem

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # [Advent of Code](https://adventofcode.com/2023/)
 
 🙃
+
+## Guidances
+
+  - Humour is subjective
+  - If my BASH looks cleaner than you're used to, don't be surprised
+    - Bash is a [super language](https://stackoverflow.com/questions/28693737/is-bash-a-programming-language)!, so don't @ me about it 😉
--- day-01/answer.sh
+++ day-01/answer.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+#!/usr/bin/env cached-nix-shell
+#!nix-shell -p bash bc gnused -i bash
 
 # Globals {
 INPUT_FILE="$(dirname "$(readlink -f ${0})")/input"
@@ -8,41 +9,48 @@ MAX_INPUT_SIZE=$(wc -l "${INPUT_FILE}" | sed 's/ .*//')
 # Usage: pretendToBeAComputerFromOldMovies
 #  Like it says
 pretendToBeAComputerFromOldMovies() {
-    echo "UN-DOING ELFIES MISCALIBRATION... PLEASE WAIT"
+    echo "UN-DOING FOOLISH ELF MISCALIBRATION." > /dev/stderr
+    sleep $((${RANDOM} % 2 + 1))
 
-    echo "COMBINING FIRST DIGIT AND LAST DIGIT"
+    echo "COMBINING FIRST DIGIT(S) AND LAST DIGIT(S). PLEASE WAIT..." > /dev/stderr
+    sleep $((${RANDOM} % 4 + 3))
+
+    echo "WARN: SOME FIRST DIGITS *ARE* LAST DIGITS. CRUNCHING..."
+    sleep $((${RANDOM} % 3 + 5))
+
+    echo -n "CALIBRATION SUM: "
 }
 
-# Usage: sanityCheck COUNT MAX
-#  Makes sure I didn't loop myself into oblivion
-sanityCheck() {
-    count="${1}"
-    max=$"${2}"
-    if [[ ${count} == ${max} ]]; then
-        echo "You goofed, Nick." > /dev/stderr
-        exit 1
-    fi
-}
+# Usage: processTheWholeFileWithSed
+#  Much easier to just get sed to do all this
+processTheWholeFileWithSed() {
+    # SECOND REGEX - HAS AT LEAST TWO DIGITS ON THE LINE
+    #   Before anything, we need to prune the lines we just did (/^[^+]/)
+    #   From the BOL ^ we should see any amount of non-digits [^[:digit]]*.
+    #   After that, we'll take any amount of [[:digit:]]\+ and \(save\) it.  We
+    #   can then accept any input [[:alnum]]*, then in some [[:alpha:]], then
+    #   a [[:digit:]] we'll save, then the rest of the alpha's in the line
+    #   until EOL, $ can be gobbled...
 
-# Usage: actuallyDoWork
-#  The answer itself
-actuallyDoWork() {
-    count=1
-    while read line; do
-        # track loop
-        sanityCheck "${count}" $((${MAX_INPUT_SIZE} + 1))
-        count=$((count + 1))
+    # FIRST REGEX - HAS ONLY ONE DIGIT ON THE LINE
+    #   The first number can also *BE* the second number, so fine, we have
+    #   not-digits [^[:digit]] as many times as we like, then a [[:digit:]]
+    #   for sure, then whatever non-digits till the EOL, $.
+    sed '
+    s/^[^[:digit:]]*\([[:digit:]]\)[^[:digit:]]*$/\1\1 +/
+    /^[^+]*/s/^[^[:digit:]]*\([[:digit:]]\)[[:alnum:]]*\([[:digit:]]\)[[:alpha:]]*$/\1\2 + /;
+    ' "${INPUT_FILE}"
 
-        # compute the answer
-        echo "${line}"
-    done < "${INPUT_FILE}"
 }
 
 # Usage: main
 #  Does the computation
 main(){
-    pretendToBeAComputerFromOldMovies
-    actuallyDoWork
+    # Unless someone says to stop, ham it up really badly
+    [[ -z ${PLZ_STOP} ]] && pretendToBeAComputerFromOldMovies
+    # Here's the actual code
+    processTheWholeFileWithSed | tee output | tr -d '\n' | sed 's/+$/\n/' | bc -l
+
 }
 
 main
