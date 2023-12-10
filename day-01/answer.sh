dfe1a5f29f62749df04018f3d366a3df77b87f3f
Nicholas Hébert <68243838+n-hebert@users.noreply.github.com>
2023-12-10 08:36:29 -0400

    Complete part 2 of day 1

--- day-01/answer.sh
+++ day-01/answer.sh
@@ -6,51 +6,90 @@ INPUT_FILE="$(dirname "$(readlink -f ${0})")/input"
 MAX_INPUT_SIZE=$(wc -l "${INPUT_FILE}" | sed 's/ .*//')
 # }
 
-# Usage: pretendToBeAComputerFromOldMovies
+usage() {
+    cat <<EOS
+Usage: $(basename "$(readlink -f "${0}")") [--part 1]
+EOS
+}
+
+# Usage: pretendToBeAComputerFromOldMovies [--part-1]
 #  Like it says
 pretendToBeAComputerFromOldMovies() {
     echo "UN-DOING FOOLISH ELF MISCALIBRATION." > /dev/stderr
     sleep $((${RANDOM} % 2 + 1))
 
-    echo "COMBINING FIRST DIGIT(S) AND LAST DIGIT(S). PLEASE WAIT..." > /dev/stderr
-    sleep $((${RANDOM} % 4 + 3))
+    echo "COMBINING FIRST DIGIT(S) AND LAST DIGIT(S). PLEASE WAIT..." >/dev/stderr
+    sleep $((${RANDOM} % 4 + 2))
 
-    echo "WARN: SOME FIRST DIGITS *ARE* LAST DIGITS. CRUNCHING..."
-    sleep $((${RANDOM} % 3 + 5))
+    echo "WARN: SOME FIRST DIGITS *ARE* LAST DIGITS. CRUNCHING..." >/dev/stderr
+    sleep $((${RANDOM} % 3 + 4))
 
-    echo -n "CALIBRATION SUM: "
+    if [[ "${1}" != "--part-1" ]]; then
+        echo "OH FOR THE LOVE OF ALL THAT IS HOLLY, ELVES, WHY ARE SOME NUMBERS WRITTEN OUT?" >/dev/stderr
+        sleep $((${RANDOM} % 2 + 2))
+
+        echo "WOW. 'TWONE'? I -- THAT'S A TWO AND A ONE? OKAY..." >/dev/stderr
+        sleep $((${RANDOM} % 2 + 2))
+    fi
+
+    echo -n "CALIBRATION SUM: " >/dev/stderr
 }
 
-# Usage: processTheWholeFileWithSed
+# Usage: processTheWholeFileWithSed --part-1
 #  Much easier to just get sed to do all this
 processTheWholeFileWithSed() {
-    # SECOND REGEX - HAS AT LEAST TWO DIGITS ON THE LINE
-    #   Before anything, we need to prune the lines we just did (/^[^+]/)
-    #   From the BOL ^ we should see any amount of non-digits [^[:digit]]*.
-    #   After that, we'll take any amount of [[:digit:]]\+ and \(save\) it.  We
-    #   can then accept any input [[:alnum]]*, then in some [[:alpha:]], then
-    #   a [[:digit:]] we'll save, then the rest of the alpha's in the line
-    #   until EOL, $ can be gobbled...
+    WORDS_AS_DIGITS=""
+    if [[ "${1}" != "--part-1" ]]; then
+        WORDS_AS_DIGITS='
+        s/one/&1&/g
+        s/two/&2&/g
+        s/three/&3&/g
+        s/four/&4&/g
+        s/five/&5&/g
+        s/six/&6&/g
+        s/seven/&7&/g
+        s/eight/&8&/g
+        s/nine/&9&/g
+        '
+    fi
+    # FIRST REGEX - DROP ALL ALPHABET CHARACTERS
+    #  We don't need them
 
-    # FIRST REGEX - HAS ONLY ONE DIGIT ON THE LINE
-    #   The first number can also *BE* the second number, so fine, we have
-    #   not-digits [^[:digit]] as many times as we like, then a [[:digit:]]
-    #   for sure, then whatever non-digits till the EOL, $.
-    sed '
-    s/^[^[:digit:]]*\([[:digit:]]\)[^[:digit:]]*$/\1\1 +/
-    /^[^+]*/s/^[^[:digit:]]*\([[:digit:]]\)[[:alnum:]]*\([[:digit:]]\)[[:alpha:]]*$/\1\2 + /;
-    ' "${INPUT_FILE}"
+    # SECOND REGEX - HAS ONLY ONE DIGIT ON THE LINE
+    # Double it
+
+    # THIRD REGEX - HAS AT LEAST TWO DIGITS ON THE LINE
+    #   From the BOL ^, take the first [[:digit:]] and \(save\) it.  We can
+    #   then accept any input, [[:digit:]]*, then a [[:digit:]] we'll save,
+    #   then the rest of the alpha's in the line until EOL, $ is seen
+
+    # FINAL REGEX - Clean the output. The last line shouldn't trail with '+'
+    sed \
+        "${WORDS_AS_DIGITS}"'
+            s/[[:alpha:]]*//g
+            s/^[[:digit:]]$/&&/
+            s/^\([[:digit:]]\)[[:digit:]]*\([[:digit:]]\)$/\1\2/
+            $s/ +//
+            ' "${INPUT_FILE}"
 
 }
 
-# Usage: main
+# Usage: main "${@}"
 #  Does the computation
 main(){
     # Unless someone says to stop, ham it up really badly
-    [[ -z ${PLZ_STOP} ]] && pretendToBeAComputerFromOldMovies
+    [[ -z ${PLZ_STOP} ]] && pretendToBeAComputerFromOldMovies "${@}"
     # Here's the actual code
-    processTheWholeFileWithSed | tee output | tr -d '\n' | sed 's/+$/\n/' | bc -l
+    parsedContent=$(processTheWholeFileWithSed "${@}")
 
+    # Ensure nothing failed before passing it into the calculator
+    if [[ ${?} -eq 0 ]]; then
+        # Echo it, split it to a file for debugging, swap all new-lines for
+        # pluses, but remove any trailing plus
+        echo "${parsedContent}" | tee output | tr '\n' '+' | sed 's/+[[:space:]]*$/\n/' | bc -l
+    else
+        echo "ERR: Failed to parse" >/dev/stderr
+    fi
 }
 
-main
+main "${@}"
